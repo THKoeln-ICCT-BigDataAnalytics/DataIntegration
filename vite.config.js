6b0830ec7b3638fd6d104ebe0771cb2b9893805d
Leo Traeger <62471620+leotraeg@users.noreply.github.com>
2025-03-05 21:27:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DataIntegration/'
+  base: "/DataIntegration"
 })
 // edit LT to deploy vite in GitHub Pages 
