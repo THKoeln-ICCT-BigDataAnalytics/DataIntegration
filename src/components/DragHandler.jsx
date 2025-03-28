b10f9d1a788bcc3ebb505a1d820cc249e8c62884
horbachM-cpu <marcel.horbach@smail.th-koeln.de>
2025-03-28 15:29:11 +0100

    Drag und Drop aktuallisierung
    funktioniert jetzt auch mit bildern und symbolen

--- src/components/DragHandler.jsx
+++ src/components/DragHandler.jsx
@@ -1,8 +1,8 @@
 import * as d3 from "d3";
 
-const enableDrag = (nodes, links, labels) => {
+const enableDrag = (nodeGroups, links, labels) => {
   function updatePositions() {
-    nodes.attr("cx", d => d.x).attr("cy", d => d.y);
+    nodeGroups.attr("transform", d => `translate(${d.x},${d.y})`);
     links.attr("x1", d => d.source.x)
          .attr("y1", d => d.source.y)
          .attr("x2", d => d.target.x)
@@ -17,20 +17,25 @@ const enableDrag = (nodes, links, labels) => {
     .on("drag", (event, d) => {
       d.x = event.x;
       d.y = event.y;
-      d3.select(event.sourceEvent.target)
-        .attr("cx", d.x)
-        .attr("cy", d.y);
       
+      // Bewege das gesamte Node-Element (egal ob circle oder image)
+      d3.select(event.sourceEvent.target.closest(".node"))
+        .attr("transform", `translate(${d.x},${d.y})`);
+
+      // Update Links
       links.each(function(link) {
         if (link.source === d) {
-          d3.select(this)
-            .attr("x1", d.x)
-            .attr("y1", d.y);
+          d3.select(this).attr("x1", d.x).attr("y1", d.y);
         }
         if (link.target === d) {
-          d3.select(this)
-            .attr("x2", d.x)
-            .attr("y2", d.y);
+          d3.select(this).attr("x2", d.x).attr("y2", d.y);
+        }
+      });
+
+      // Update Labels
+      labels.each(function(label) {
+        if (label === d) {
+          d3.select(this).attr("x", d.x + 10).attr("y", d.y + 5);
         }
       });
     })
