ef7f2c2dcf3092f0e20f941dcc01bcf479f9217b
horbachM-cpu <marcel.horbach@smail.th-koeln.de>
2025-04-10 23:47:29 +0200

    Erweiterung der Graph Node Klasse zur Vorbereitung von Verlinkungen
    Nun kann Graph Nodes Verlinkungen organisieren. Restliche Klassen werden noch auf Merge Konflikte untersucht und später commited

--- src/components/GraphNode.jsx
+++ src/components/GraphNode.jsx
@@ -17,8 +17,12 @@ class GraphNode {
     this.FORMULA_agree = 0;
     this.overall_agreement = 0;
     this.predict_linkability = 0;
-    this.confusion = 0;
+    this.confusion = 0;  // Korrigierter Wert
     this.v = 0;
+    this.highestlink = 0;
+    this.secondhighestlink = 0;
+    this.thirdhighestlink = 0;
+    this.allLinks = [];  // Neues Attribut als leeres Array
   }
 
   addChild(childNode) {
@@ -71,6 +75,29 @@ class GraphNode {
 
   setV(value) {
     this.v = value;
+  }
+
+  setHighestLink(value) {
+    this.highestlink = value;
+  }
+
+  setSecondHighestLink(value) {
+    this.secondhighestlink = value;
+  }
+
+  setThirdHighestLink(value) {
+    this.thirdhighestlink = value;
+  }
+
+  // Methode zum Hinzufügen eines einzelnen Links
+  addLink(linkNode) {
+    this.allLinks.push(linkNode);
+  }
+
+  // Neue Methode zum Hinzufügen einer Teilmenge aus einem Array
+  addLinksFromArray(linkArray, filterCondition = () => true) {
+    const filteredLinks = linkArray.filter(filterCondition);
+    this.allLinks.push(...filteredLinks);
   }
 }
 
